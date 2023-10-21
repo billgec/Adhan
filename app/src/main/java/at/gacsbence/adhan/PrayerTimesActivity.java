9f799676db7a80dc3f4e812f46be0b12b71f6710
Bence Gacs <99565818+billgec@users.noreply.github.com>
2023-10-21 23:02:57 +0200

    update activities

--- app/src/main/java/at/gacsbence/adhan/PrayerTimesActivity.java
+++ app/src/main/java/at/gacsbence/adhan/PrayerTimesActivity.java
@@ -1,11 +1,20 @@
 package at.gacsbence.adhan;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
 /**
  * Prayer times activity
- * Created by Bence on 2018. 02. 25..
+ * Created by Bence on 21.10.2023
  * Project: adhan
  * Description: Display the full prayer times schedule for the day, week, or month. Users can browse upcoming prayer times and view past prayer times.
  */
+public class PrayerTimesActivity extends AppCompatActivity {
 
-public class PrayerTimesActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prayer_times);
+    }
 }
--- app/src/main/java/at/gacsbence/adhan/SettingsActivity.java
+++ app/src/main/java/at/gacsbence/adhan/SettingsActivity.java
@@ -1,10 +1,21 @@
 package at.gacsbence.adhan;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
 /**
  * Settings activity
- * Created by Bence on 2018. 02. 25..
+ * Created by Bence on 21.10.2023
  * Project: adhan
  * Description: Allow users to configure their preferences, including selecting Adhan sounds, notification settings, and location settings.
  */
-public class SettingsActivity {
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
 }
